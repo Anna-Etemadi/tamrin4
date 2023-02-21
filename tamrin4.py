dcec08224eb43dc4b9654235e8ffe3712ade3107
Anna-Etemadi <124399415+Anna-Etemadi@users.noreply.github.com>
2023-02-21 21:51:50 +0330

    Add files via upload

---
+++ tamrin4.py
@@ -0,0 +1,12 @@
+set1={1,2,3,4,5,6,7,8,9,10}
+set1=list(set1)
+set1.clear()
+set1=[5,4,6,7,1,2,4,6,7,8]
+print(set1)
+print(set1[3:6])
+set1=set(set1)
+print(set1)
+del set1
+txt1="Hi I am Anna"
+list2=txt1.split()
+print(list2)
